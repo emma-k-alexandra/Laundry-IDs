16b7570effe4d1dd9a246afecb4712bbe447314a
Emma K Alexandra <emma@emma.sh>
2021-02-15 15:37:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Scrape CSC Service Works CVA IDs
+A simple Cypress scraper for CSC Service Work's CVA IDs
+
+## Install
+```bash
+yarn
+```
+
+## Run
+```bash
+ELECTRON_ENABLE_LOGGING=1 npx cypress run > laundry.log 2>&1
+```
+
+## Output
+Found CVA IDs can be found in logs by searching for `[Found]`.
---
+++ cypress/support/index.js
@@ -0,0 +1,38 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// process.env.ELECTRON_ENABLE_LOGGING = 1
+
+Cypress.on('window:before:load', (win) => {
+  Cypress.log({
+      name: 'console.log',
+      message: 'wrap on console.log',
+  });
+
+  // pass through cypress log so we can see log inside command execution order
+  win.console.log = (...args) => {
+      Cypress.log({
+          name: 'console.log',
+          message: args,
+      });
+  };
+});
+
+Cypress.on('log:added', (options) => {
+  if (options.instrument === 'command') {
+      // eslint-disable-next-line no-console
+      console.log(options.message)
+  }
+});
